341ca650848347f79e3f9402bb20e23f21bca671
jeffng5 <jeffreykng@hotmail.com>
2024-01-18 16:12:25 -0500

    first commit of todo's

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import './App.css';
+import TodoList from "./TodoList"
+
+
+
+
+function App(){
+
+
+return(
+<div className='App'>
+
+<h2><TodoList /></h2>
+
+
+
+</div>
+)
+}
+
+export default App;
